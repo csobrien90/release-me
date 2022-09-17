ed617b5ada1344a70ad89d6e3b668502d624c8c8
Chad OBrien <obrien.music@gmail.com>
2022-09-17 19:47:39 -0400

    place and animate logout

--- src/components/gravatarLogin.js
+++ src/components/gravatarLogin.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GravatarLogin = ({ userName }) => {
+	const [isLogoutExpanded, setIsLogoutExpanded] = useState('');
+	
+	const toggleMenu = () => {
+		if (isLogoutExpanded === '') {
+			setIsLogoutExpanded('isExpanded');
+		} else {
+			setIsLogoutExpanded('');
+		}
+	}
 
 	const logout = () => {
 		localStorage.removeItem("accessToken");
@@ -9,10 +18,12 @@ const GravatarLogin = ({ userName }) => {
 	}
 
 	return (
-		<p>
-			Welcome, {userName}
-			<button onClick={() => logout()}>Logout</button>
-		</p>
+		<header id='logout-wrapper'>
+			<p id='logout' onClick={() => toggleMenu()}>
+				Welcome, {userName}
+				<button onClick={() => logout()} className={isLogoutExpanded}>Logout</button>
+			</p>
+		</header>
 	)
 }
 
